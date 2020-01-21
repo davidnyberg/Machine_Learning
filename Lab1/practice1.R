c6093cfddc5c88edebb8d55a7ca2ed1176bdaa31
davidnyberg <dnyberg95@gmail.com>
2020-01-21 21:43:30 +0100

    Add files via upload

---
+++ Lab1/practice1.R
@@ -0,0 +1,20 @@
+v1<-rep(3,5)
+v1
+sapply(v1, log)
+
+f1<-c("Man", "Woman")
+f1
+
+x<-c(1,3)
+y<-c("M", "F")
+z<-data.frame(x,y)
+z
+
+df = read.csv()
+
+myfun<-function(x)
+{
+  return(1)
+}
+
+myfun(5)
